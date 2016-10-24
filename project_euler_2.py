7c81f540882d820cce5f178ac24326301a4675c2
smlawson <sxlawson@gmail.com>
2016-10-23 23:00:47 -0400

    added comments

--- project_euler_2.py
+++ project_euler_2.py
@@ -1,16 +1,16 @@
-m = 1
-n = 2
-s = 2
-i = 0
+i = 0 #primary index
+m = 1 #first secondary index
+n = 2 #second secondary index
+s = 2 #sum of even-valued terms of the Fibonacci sequence whose values do not exceed four million 
 
-while i <= 4000000:
-    i = m + n
-    if i%2 == 0:
-        s = s + i
-    m = i
-    i = m + n
-    if i%2 == 0:
-        s = s + i
-    n = i
+while i <= 4000000: #considering only terms that do not exceed four million
+    i = m + n #next term of the Fibonacci sequence
+    if i%2 == 0: #test to see if i is even
+        s = s + i #only add to sum if i is indeed even
+    m = i #moving up one 'rung' of the Fibonacci sequence
+    i = m + n #next term of the Fibonacci sequence
+    if i%2 == 0: #test to see if i is even
+        s = s + i #only add to sum if i is indeed even
+    n = i #moving up one 'rung' of the Fibonacci sequence
 
-print(s)
+print(s) #display final sum (this is the 'answer')
