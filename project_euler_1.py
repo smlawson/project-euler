79d94e600973011da0c9e892233fe8a7c5495a01
smlawson <sxlawson@gmail.com>
2016-10-23 23:09:19 -0400

    added comments

--- project_euler_1.py
+++ project_euler_1.py
@@ -1,8 +1,9 @@
-i = 1
-s = 0
-while i < 1000:
-    if i%3 == 0 or i%5 ==0:
-        s = s + i
-    i = i + 1
+i = 1 #primary index
+s = 0 #sum of all multiples of 3 or 5 below 1000
 
-print(s)
+while i < 1000: #only considering natural numbers below 1000
+    if i%3 == 0 or i%5 ==0: #test to see if i is a multiple of 3 or 5
+        s = s + i #only multiples of 3 or 5 are considered in total sum
+    i = i + 1 #raise i by 1 and test again
+
+print(s) #display final sum (this is the 'answer')
